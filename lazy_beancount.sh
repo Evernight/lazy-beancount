62d5fbd0ac194c58009a5838f0e97be9552a1136
Vasily Medyanikov <vasily.medyanikov@gmail.com>
2024-06-25 16:55:48 +0200

    add launcher

---
+++ lazy_beancount.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+docker rm lazybean;
+docker run -it \
+    -v $PWD:/workspace \
+    -v $PWD/tmp:/home/beancount-user/ \
+    -p 5000:5000 \
+    -p 8101:8101 \
+    -p 8777:8501 \
+    --name lazybean \
+    lazy-beancount
