9c4f9f837783c03a92559a31741e22f006cbf109
Evernight <786357+Evernight@users.noreply.github.com>
2026-01-30 20:29:49 +0100

    beanhub integration

--- src/scripts/run_daemons.sh
+++ src/scripts/run_daemons.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+BEANHUB_FORMS_PORT=${BEANHUB_FORMS_PORT:-8310}
+# BEANHUB_FORMS_FILE=${BEANHUB_FORMS_FILE:-".beanhub/forms.yaml"}
+
+cd /workspace
+
 echo "================================================================"
 echo "Lazy Beancount is available on http://$LAZY_BEANCOUNT_HOST:$LAZY_BEANCOUNT_PORT/"
 echo "(configurable via LAZY_BEANCOUNT_PORT environment variable)"
 echo ""
 echo "Fava available on http://$LAZY_BEANCOUNT_HOST:$FAVA_PORT/"
 echo "Beancount-import available on http://$LAZY_BEANCOUNT_HOST:$BEANCOUNT_IMPORT_PORT/"
+echo "BeanHub Forms available on http://$LAZY_BEANCOUNT_HOST:$BEANHUB_FORMS_PORT/"
 echo "================================================================"
 echo ""
 
@@ -15,6 +21,7 @@ python3 -m beancount_importers.beancount_import_run \
     --journal_file main.bean \
     --importers_config_file importers_config.yml \
     2>&1 | tee lazy-beancount.log &
+bh form server --host 0.0.0.0 --port $BEANHUB_FORMS_PORT 2>&1 | tee lazy-beancount.log &
 streamlit run /beancount/streamlit_frontend/frontend.py \
     --server.address 0.0.0.0 \
     --server.headless "true" \
